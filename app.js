2ceedcb4d66893f320bb18610b5efe3e3493ef6f
nityajha <31059295+nityajha@users.noreply.github.com>
2017-10-25 16:27:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function setPixelColor(pixel){
 	pixel.style.backgroundColor = penColor;
 }
 
+var i = 2;
+var j= 2;
+var r = $("#art");
+var c = $(".row");
+var rows;
+rows = $(<'div class = "pixel" onclick = "setPixelColor(this)"></div>');
+
+for ( var a=0; a<i; a++){
+	r.append(rows);
+	for (var b=0; b<j; b++){
+		c.append(rows);
+	}
+}
+
+	
