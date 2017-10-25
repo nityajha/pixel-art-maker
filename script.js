1ecf9f1913a3e1624f603f544320d42dbac6ce54
nityajha <31059295+nityajha@users.noreply.github.com>
2017-10-25 19:33:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var penColor = "black";
+function setPenColor(pen){
+	penColor = pen;
+}
+function setPixelColor(pixel){
+	pixel.style.backgroundColor = penColor;
+}
+
+var i = document.getElementById("rownum").value;
+var j= document.getElementById("colnum").value;
+var routput = ' ';
+var coutput = ' ';
+
+for ( var a=4; a<i; a++){
+	routput += '<div class = "row">';
+	document.getElementById('art').innerHTML = routput;
+	for (var b=4; b<j; b++){
+		coutput += '<div class="pixel" onclick = "setPixelColor(this)"></div>';
+		document.getElementByClass('row').innerHTML = coutput;
+		
+	}
+}
