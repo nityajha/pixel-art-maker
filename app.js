b56e497da27d913f771d7ada915c21f8ef7f2725
nityajha <31059295+nityajha@users.noreply.github.com>
2017-10-25 17:52:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,18 +7,20 @@ function setPixelColor(pixel){
 	pixel.style.backgroundColor = penColor;
 }
 
-var i = 2;
-var j= 2;
-var r = $("#art");
-var c = $(".row");
-var rows;
-rows = $(<'div class = "pixel" onclick = "setPixelColor(this)"></div>');
+var i = document.getElementById("rownum").value;
+var j= document.getElementById("colnum").value;
+var routput = ' ';
+var coutput = ' ';
 
-for ( var a=0; a<i; a++){
-	r.append(rows);
-	for (var b=0; b<j; b++){
-		c.append(rows);
+for ( var a=2; a<i; a++){
+	routput += '<div class = "row">';
+	document.getElementById('art').innerHTML = routput;
+	for (var b=2; b<j; b++){
+		coutput += '<div class="pixel" onclick = "setPixelColor(this)"></div>';
+		document.getElementByClass('row').innerHTML = coutput;
+		
 	}
 }
+
 
 	
